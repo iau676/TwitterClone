c57e75d60e15bf920bf2c674a73adc8a98a57f6a
iau676 <ibrahimuysal676@gmail.com>
2023-01-29 21:53:49 +0300

    add TweetViewModel

---
+++ TwitterClone/ViewModel/TweetViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  TweetViewModel.swift
+//  TwitterClone
+//
+//  Created by ibrahim uysal on 29.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct TweetViewModel {
+    
+    let tweet: Tweet
+    let user: User
+    
+    var profileImageUrl: URL? {
+        return tweet.user.profileImageUrl
+    }
+    
+    var userInfoText: NSAttributedString {
+        let title = NSMutableAttributedString(string: user.fullname,
+                                              attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        
+        title.append(NSAttributedString(string: " @\(user.username)",
+                                        attributes: [.font: UIFont.systemFont(ofSize: 14),
+                                                     .foregroundColor: UIColor.lightGray]))
+        
+        return title
+    }
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+        self.user = tweet.user
+    }
+}
