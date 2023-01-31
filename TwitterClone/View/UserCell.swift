9b58d0e700667ae2d833992a8f6e1c006121cc5e
iau676 <ibrahimuysal676@gmail.com>
2023-01-31 17:59:25 +0300

    add UserCell

---
+++ TwitterClone/View/UserCell.swift
@@ -0,0 +1,62 @@
+//
+//  UserCell.swift
+//  TwitterClone
+//
+//  Created by ibrahim uysal on 31.01.2023.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    //MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.setDimensions(height: 40, width: 40)
+        iv.layer.cornerRadius = 40 / 2
+        iv.layer.masksToBounds = true
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.text = "Fullname"
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .white
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self)
+        profileImageView.anchor(left: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        stack.distribution = .fillEqually
+        
+        addSubview(stack)
+        stack.centerY(inView: self)
+        stack.anchor(left: profileImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
