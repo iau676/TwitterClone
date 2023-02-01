eb7fb501708f69ac6501bdcf52f1707f21932944
iau676 <ibrahimuysal676@gmail.com>
2023-02-01 03:38:13 +0300

    add TweetController

---
+++ TwitterClone/View/TweetHeader.swift
@@ -0,0 +1,211 @@
+//
+//  TweetHeader.swift
+//  TwitterClone
+//
+//  Created by ibrahim uysal on 1.02.2023.
+//
+
+import UIKit
+
+class TweetHeader: UICollectionReusableView {
+    
+    //MARK: - Properties
+    
+    private lazy var profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.setDimensions(height: 48, width: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.layer.masksToBounds = true
+        iv.backgroundColor = .twitterBlue
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(profileImagePressed))
+        iv.addGestureRecognizer(tap)
+        iv.isUserInteractionEnabled = true
+        
+        return iv
+    }()
+    
+    private let fullnameLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.text = "Water Lily"
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .lightGray
+        label.text = "@waterlily"
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.numberOfLines = 0
+        label.text = "some test caption some test caption some test caption"
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+       let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .left
+        label.text = "03:10 AM - 01/02/2023"
+        return label
+    }()
+    
+    private lazy var optionsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .lightGray
+        button.setImage(UIImage(named: "down_arrow_24pt"), for: .normal)
+        button.addTarget(self, action: #selector(showActionSheet), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var retweetsLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "2 Retweets"
+        return label
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "0 Likes"
+        return label
+    }()
+    
+    private lazy var statsView: UIView = {
+       let view = UIView()
+        
+        let divider1 = UIView()
+        divider1.backgroundColor = .systemGroupedBackground
+        view.addSubview(divider1)
+        divider1.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                        paddingLeft: 8, height: 1.0)
+        
+        let stack = UIStackView(arrangedSubviews: [retweetsLabel, likesLabel])
+        stack.axis = .horizontal
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        stack.centerY(inView: view)
+        stack.anchor(left: view.leftAnchor, paddingLeft: 16)
+        
+        let divider2 = UIView()
+        divider2.backgroundColor = .systemGroupedBackground
+        view.addSubview(divider2)
+        divider2.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor,
+                        paddingLeft: 8, height: 1.0)
+        
+        return view
+    }()
+    
+    private lazy var commentButton: UIButton = {
+       let button = createButton(withImageName: "comment")
+        button.addTarget(self, action: #selector(commentPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var retweetButton: UIButton = {
+       let button = createButton(withImageName: "retweet")
+        button.addTarget(self, action: #selector(retweetPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var likeButton: UIButton = {
+       let button = createButton(withImageName: "like")
+        button.addTarget(self, action: #selector(likePressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+       let button = createButton(withImageName: "share")
+        button.addTarget(self, action: #selector(sharePressed), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+                
+        let labelStack = UIStackView(arrangedSubviews: [fullnameLabel, usernameLabel])
+        labelStack.axis = .vertical
+        labelStack.spacing = -6
+        
+        let stack = UIStackView(arrangedSubviews: [profileImageView, labelStack])
+        stack.spacing = 12
+        
+        addSubview(stack)
+        stack.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 16)
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: stack.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16)
+        
+        addSubview(dateLabel)
+        dateLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, paddingTop: 20, paddingLeft: 16)
+        
+        addSubview(optionsButton)
+        optionsButton.centerY(inView: stack)
+        optionsButton.anchor(right: rightAnchor, paddingRight: 8)
+        
+        addSubview(statsView)
+        statsView.anchor(top: dateLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 20, height: 40)
+        
+        let actionStack = UIStackView(arrangedSubviews: [commentButton, retweetButton, likeButton, shareButton])
+        actionStack.spacing = 72
+        actionStack.distribution = .fillEqually
+        
+        addSubview(actionStack)
+        actionStack.centerX(inView: self)
+        actionStack.anchor(bottom: bottomAnchor, paddingBottom: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Selectors
+    
+    @objc func profileImagePressed() {
+        print("DEBUG: profileImagePressed")
+    }
+    
+    @objc func showActionSheet() {
+        print("DEBUG: showActionSheet")
+    }
+    
+    @objc func commentPressed() {
+        print("DEBUG: commentPressed")
+    }
+    
+    @objc func retweetPressed() {
+        print("DEBUG: retweetPressed")
+    }
+    
+    @objc func likePressed() {
+        print("DEBUG: likePressed")
+    }
+    
+    @objc func sharePressed() {
+        print("DEBUG: sharePressed")
+    }
+    
+    //MARK: - Helpers
+    
+    func createButton(withImageName imageName: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.tintColor = .darkGray
+        button.setDimensions(height: 20, width: 20)
+        return button
+    }
+}
