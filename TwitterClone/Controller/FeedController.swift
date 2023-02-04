7301458ea30062dc4d3e4b801373c22189460264
iau676 <ibrahimuysal676@gmail.com>
2023-02-04 12:00:16 +0300

    update feed by following

--- TwitterClone/Controller/FeedController.swift
+++ TwitterClone/Controller/FeedController.swift
@@ -33,20 +33,30 @@ class FeedController: UICollectionViewController {
     //MARK: - API
     
     func fetchTweets() {
+        collectionView.refreshControl?.beginRefreshing()
         TweetService.shared.fetchTweets { tweets in
-            print("DEBUG: Number of tweets is \(tweets.count)")
-            self.tweets = tweets
-            self.checkIfUserLikedTweets(tweets)
+            self.collectionView.refreshControl?.endRefreshing()
+            self.tweets = tweets.sorted(by: { $0.timestamp > $1.timestamp })
+            self.checkIfUserLikedTweets()
         }
     }
     
-    func checkIfUserLikedTweets(_ tweets: [Tweet]) {
-        for (index, tweet) in tweets.enumerated() {
+    func checkIfUserLikedTweets() {
+        self.tweets.forEach { tweet in
             TweetService.shared.checkIfUserLikedTweet(tweet) { didLike in
                 guard didLike == true else { return }
-                self.tweets[index].didLike = true
+                
+                if let index = self.tweets.firstIndex(where: { $0.tweetID == tweet.tweetID }) {
+                    self.tweets[index].didLike = true
+                }
             }
         }
+    }
+    
+    //MARK: - Selectos
+    
+    @objc func handleRefresh() {
+        fetchTweets()
     }
     
     //MARK: - Helpers
@@ -61,6 +71,10 @@ class FeedController: UICollectionViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.setDimensions(height: 44, width: 44)
         navigationItem.titleView = imageView
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView.refreshControl = refreshControl
     }
     
     private func configureLeftBarButton() {
